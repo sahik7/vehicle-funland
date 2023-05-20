9e8dc1914ac642b305a6c024eccf4869c5311a5e
sahik7 <sahikahsan7@gmail.com>
2023-05-20 14:37:55 +0600

    single product container design half complete

--- src/pages/AllToys/SingleToy.jsx
+++ src/pages/AllToys/SingleToy.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 
-const SingleToy = ({vehicle}) => {
-    const {Image, Seller} = vehicle;
+const SingleToy = ({ vehicle }) => {
+    const { Image, Seller, ToyName, AvailableQuantity, Price } = vehicle;
     return (
         <div>
-            <div className="border-4 border-black single-product-container grid grid-cols-6">
-            <div className="col-span-3">
-            <img className="w-2/5 mx-auto" src={Image} alt="" />
-            </div>
-            <div className="border col-span-3">
-                <h4 className='font-light'>{Seller}</h4>
-            </div>
+            <div className="border-black border-4 single-product-container">
+                <div className="grid grid-cols-8">
+                    <div className="col-span-3">
+                        <img className="" src="for-test.jpg" alt="" />
+                    </div>
+                    <div className=" col-span-5 p-4">
+                        <h4 className='font-semibold text-sm text-secondary'>{Seller}</h4>
+                        <h2 className='font-bold text-xl py-2'>{ToyName}</h2>
+                        <h2 className=' text-xl py-2'></h2>
+                    </div>
+                </div>
+                <div className='h-12 grid grid-cols-6'>
+                    {/* View Details */}
+                    <div className=' flex justify-center border items-center col-span-4'>
+                        <button className='font-bold w-full h-full bg-black text-white'>View Details</button>
+                    </div>
+                    {/* Price */}
+                    <div className=' col-span-2 flex justify-center items-center'>
+                        <p className='font-bold px-12 py-2 '>{Price}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -11,8 +11,9 @@ const Banner = () => {
                         World of  Vehicle Toys!</h2>
                     {/* Let's Shop Button */}
                     <div className="relative group lg:w-1/4 mx-auto mt-32">
-                        <div className='absolute left-9 -bottom-3 duration-300 group-hover:left-8 group-hover:-bottom-4 border-black lg:py-3 lg:px-8 border-4 bg-white text-white'>Let's Shop</div>
-                        <button onClick={() => navigate("/allToys")} className='py-3 w-3/5 text-sm lg:text-base absolute group-hover:-top-12 group-hover:left-12 -top-10 -translate-x-1/2 left-1/2 px-8 bg-black text-white lg:font-semibold duration-300 
+                        <div className='absolute lg:-bottom-3 lg:py-3 -bottom-0 duration-300 group-hover:left-28 group-hover:-bottom-4 border-black
+                        -translate-x-1/2 left-1/2 px-14 border-4 group-hover:bg-white bg-black text-white'>Shop</div>
+                        <button onClick={() => navigate("/allToys")} className='py-3 w-3/5 text-sm lg:text-base absolute group-hover:-top-12 -top-10 -translate-x-1/2 left-1/2 px-8 bg-black text-white lg:font-semibold duration-300 
                     '> Let's Shop</button>
 
                     </div>
