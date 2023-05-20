0a9269b63d9d10be95d18fa640fdbe37e2033713
sahik7 <sahikahsan7@gmail.com>
2023-05-20 16:48:58 +0600

    search bar style added in the alltoys page

--- src/pages/AllToys/SingleToy.jsx
+++ src/pages/AllToys/SingleToy.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const SingleToy = ({ vehicle }) => {
-    const { Image, Seller, ToyName, AvailableQuantity, Price } = vehicle;
+    const { Image, Seller, ToyName, AvailableQuantity, Price, SubCategory } = vehicle;
+
+    console.log(vehicle)
     return (
         <div>
             <div className="border-black border-4 single-product-container">
@@ -10,14 +12,17 @@ const SingleToy = ({ vehicle }) => {
                         <img className="" src="for-test.jpg" alt="" />
                     </div>
                     <div className=" col-span-5 p-4">
-                        <h4 className='font-semibold text-sm text-secondary'>{Seller}</h4>
-                        <h2 className='font-bold text-xl py-2'>{ToyName}</h2>
-                        <h2 className=' text-xl py-2'></h2>
+                        <h4 className='font-medium text-sm text-secondary mt-2'>{Seller}</h4>
+                        <h2 className='font-bold text-2xl py-2'>{ToyName}</h2>
+                        <div>
+                            <p className='bg-red-100 my-2 text-center text-[13px] py-1 w-[38%] text-primary'>{SubCategory}</p>
+                            <p className='mt-4 font-light'>In Stock -  {AvailableQuantity}</p>
+                        </div>
                     </div>
                 </div>
-                <div className='h-12 grid grid-cols-6'>
+                <div className='h-12 mt-8 grid grid-cols-6'>
                     {/* View Details */}
-                    <div className=' flex justify-center border items-center col-span-4'>
+                    <div className=' flex justify-center border-4 border-white items-center col-span-4'>
                         <button className='font-bold w-full h-full bg-black text-white'>View Details</button>
                     </div>
                     {/* Price */}
