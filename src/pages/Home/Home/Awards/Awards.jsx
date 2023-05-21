66bc06d9a297a6675b9b78e2942c40f4d22ad61d
sahik7 <sahikahsan7@gmail.com>
2023-05-21 16:50:48 +0600

    mobile responsive done for home page

--- src/pages/Home/Home/Awards/Awards.jsx
+++ src/pages/Home/Home/Awards/Awards.jsx
@@ -4,12 +4,12 @@ const Awards = () => {
     return (
         <div>
             <div className="border rounded py-8">
-                <h1 className='text-3xl font-bold mx-auto border-b-4 pb-4 w-1/5 border-b-secondary'>Our Awards</h1>
-                <div className='flex  w-3/4 mx-auto my-10 justify-between'>
-                    <div><img className='w-28' src="https://i.ibb.co/8PZTnBD/award3.jpg" alt="" /><p className='text-gray-400 text-center mt-4'>Parents & Child</p></div>
-                    <div><img className='w-36' src="https://i.ibb.co/kDdHsWq/award2.jpg" alt="" /><p className='text-gray-400 text-center mt-4'>Best Toys</p></div>
-                    <div><img className='w-36' src="https://i.ibb.co/smHptvz/award1.jpg" alt="" /><p className='text-gray-400 text-center mt-4'>TOTY</p></div>
-                    <div><img className='w-28' src="https://i.ibb.co/J36pG26/award4.jpg" alt="" /><p className='text-gray-400 text-center mt-4'>Toy Mendous</p></div>
+                <h1 className='text-3xl text-center font-bold mx-auto border-b-4 pb-4 lg:w-1/5 w-4/6 border-b-secondary'>Our Awards</h1>
+                <div className='lg:flex  w-3/4 mx-auto my-10 justify-between'>
+                    <div><img className='lg:w-28 lg:mx-none mx-auto' src="https://i.ibb.co/8PZTnBD/award3.jpg" alt="" /><p className='text-gray-400 text-center mt-4 mb-4 lg:mb-0'>Parents & Child</p></div>
+                    <div><img className='lg:w-36 lg:mx-none mx-auto' src="https://i.ibb.co/kDdHsWq/award2.jpg" alt="" /><p className='text-gray-400 text-center mt-4 mb-4 lg:mb-0'>Best Toys</p></div>
+                    <div><img className='lg:w-36 lg:mx-none mx-auto' src="https://i.ibb.co/smHptvz/award1.jpg" alt="" /><p className='text-gray-400 text-center mt-4 mb-4 lg:mb-0'>TOTY</p></div>
+                    <div><img className='lg:w-28 lg:mx-none mx-auto' src="https://i.ibb.co/J36pG26/award4.jpg" alt="" /><p className='text-gray-400 text-center mt-4 mb-4 lg:mb-0'>Toy Mendous</p></div>
                 </div>
             </div>
         </div>
--- src/pages/Home/OurTeam/OurTeam.jsx
+++ src/pages/Home/OurTeam/OurTeam.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const OurTeam = () => {
     return (
         <div>
-            <div className="flex border rounded">
-                <div className=" w-1/2 p-12 text-center">
+            <div className="lg:flex border rounded">
+                <div className=" lg:w-1/2 p-12 text-center">
                     <h2 className='text-3xl font-bold border-b-4 border-secondary pb-6 w-1/2 mx-auto'>Our Team</h2>
                     <img className='mx-auto mt-10' src="https://i.ibb.co/yh5txH7/team.jpg" alt="" />
                 </div>
-                <div className=" w-1/2 p-12 text-center text-gray-600">
+                <div className=" lg:w-1/2 p-12 text-center text-gray-600">
                     <p>At vehicle Funland, we are fueled by a team of passionate and creative individuals who are dedicated to bringing joy and imagination into the lives of children. We believe that toys have the power to inspire, educate, and create unforgettable memories.
                     </p>
                     <br />
